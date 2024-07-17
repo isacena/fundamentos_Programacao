effcb427d771dfa5229458794aa3cf3f709313a2
Isabely <158523593+isacena@users.noreply.github.com>
2024-07-17 19:21:12 -0300

    soma personalizada - atividade - 16/07

---
+++ funcaominha.py
@@ -0,0 +1,16 @@
+def produto_empresa (product1, product2, product3):
+    soma = (p1,p2,p3)
+    return (soma)
+print ('primeiro produto: 10 reais')
+print ('segundo produto: 11 reais')
+print ('terceiro produto: 12 reais')
+
+p1 = int(input ('Digite o valor do primeiro produto: '))
+p2 = int(input('Digite o valor do segundo produto: '))
+p3 = int(input('Digite o valor do terceiro produto: '))
+
+print ('')
+
+soma_final = produto_empresa (p1,p2,p3)
+
+print('O valor da sua compra deu:', p1 + p2 + p3)
